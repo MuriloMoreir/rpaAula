5655163b1017ee57ee3975527c777e26c95f0293
MuriloMoreir <124629928+MuriloMoreir@users.noreply.github.com>
2024-06-20 00:12:21 -0300

    Create data.py

---
+++ data.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+
+
+# Obter a data e hora atual
+dataAtual = datetime.now()
+
+# Convertendo para String
+dataConvertida = dataAtual.strftime('%Y-%m-%d')
+horaAtual = dataAtual.strftime('%H:%M:%S')
+
+print('Data atual: ', dataConvertida)
+print('Hora atual: ', horaAtual)
