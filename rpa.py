33914508c4182714332efb4291de71483dff6c8e
MuriloMoreir <124629928+MuriloMoreir@users.noreply.github.com>
2024-06-20 00:04:41 -0300

    Update rpa.py

--- rpa.py
+++ rpa.py
@@ -4,22 +4,24 @@ from time import sleep
 from datetime import datetime
 from selenium.webdriver.chrome.options import Options
 import pandas as pd
-
+import psycopg2
 
 # Obter a data e hora atual
 dataAtual = datetime.now()
 
 # Convertendo para String
-dataConvertida = dataAtual.strftime('%Y-%m-%d %H:%M:%S')
+dataConvertida = dataAtual.strftime('%Y-%m-%d')
+horaAtual = dataAtual.strftime('%H:%M:%S')
 
-print('Data e Hora atual: ', dataConvertida)
+print('Data atual: ', dataConvertida)
+print('Hora atual: ', horaAtual)
 
 # Entrando no site
 chrome_options = Options()
 chrome_options.add_argument("--headless")
 driver = webdriver.Chrome(options = chrome_options)
 
-driver.get("https://www.google.com.br/search?q=d%C3%B3lar&sca_esv=e538a6082b46d93a&sca_upv=1&hl=pt-BR&sxsrf=ADLYWIIn2256gESQaJCnxeEcPG76Z9VcgA%3A1718852130869&source=hp&ei=IppzZrnVMpOv1sQP4tKk2Ac&iflsig=AL9hbdgAAAAAZnOoMiGVZGuEFKDBshlJMQ4RYH1aHkhW&ved=0ahUKEwi57reVl-mGAxWTl5UCHWIpCXsQ4dUDCBU&uact=5&oq=d%C3%B3lar&gs_lp=Egdnd3Mtd2l6IgZkw7NsYXIyDxAjGIAEGCcYigUYRhiCAjILEAAYgAQYsQMYgwEyCxAAGIAEGLEDGIMBMgsQABiABBixAxiDATILEAAYgAQYsQMYgwEyCxAAGIAEGLEDGIMBMgsQABiABBixAxiDATIIEAAYgAQYsQMyCxAAGIAEGLEDGIMBMgsQABiABBixAxiDAUjeBlAAWKYEcAB4AJABAJgBbqABgwSqAQMxLjS4AQPIAQD4AQGYAgWgApEEwgIKECMYgAQYJxiKBcICDhAuGIAEGLEDGIMBGIoFwgIREC4YgAQYsQMY0QMYgwEYxwHCAgsQLhiABBjRAxjHAZgDAJIHAzEuNKAHyyo&sclient=gws-wiz")
+driver.get("https://www.google.com.br/search?q=d%C3%B3lar")
 sleep(2)
 
 # Pegando o valor do dólar
@@ -27,3 +29,31 @@ dolar = driver.find_element(By.CSS_SELECTOR, ('.DFlfde.SwHCTb'))
 dolar_site = dolar.text
 dolar_site = float(dolar_site.replace(",","."))
 print('Dólar: ', dolar_site)
+
+
+# Conectando com o DB
+conexao = psycopg2.connect(database = "dbCotacao",
+                           host = "pg-3f7b996d-muriloolimora971.f.aivencloud.com",
+                           user = "avnadmin",
+                           password = "AVNS_BjsAizQig1olY9q0atk",
+                           port = "23734")
+
+print(conexao.info)
+print(conexao.status)
+
+# Conexão com o cursor
+cursor = conexao.cursor()
+
+# Acionando Procedure
+cursor.execute("call inserir_cotacao_dolar(%s, %s, %s)", (dataConvertida, horaAtual, dolar_site))
+
+# Comitando
+conexao.commit()
+print("Dados inseridos com sucesso !!!")
+
+# Desligando a conexão com o banco
+cursor.close()
+conexao.close()
+
+# Fechando navegador
+driver.quit()
