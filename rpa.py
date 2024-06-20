ab0d49d5b4ba93ce7fe99728b93867cdb223a848
MuriloMoreir <124629928+MuriloMoreir@users.noreply.github.com>
2024-06-20 00:01:13 -0300

    Update rpa.py

--- rpa.py
+++ rpa.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from time import sleep
 from datetime import datetime
+from openpyxl import load_workbook
 from selenium.webdriver.chrome.options import Options
 import pandas as pd
 
@@ -19,7 +20,7 @@ chrome_options = Options()
 chrome_options.add_argument("--headless")
 driver = webdriver.Chrome(options = chrome_options)
 
-driver.get("https://www.google.com.br/search?q=d%C3%B3lar")
+driver.get("https://www.google.com.br/search?q=d%C3%B3lar&sca_esv=e538a6082b46d93a&sca_upv=1&hl=pt-BR&sxsrf=ADLYWIIn2256gESQaJCnxeEcPG76Z9VcgA%3A1718852130869&source=hp&ei=IppzZrnVMpOv1sQP4tKk2Ac&iflsig=AL9hbdgAAAAAZnOoMiGVZGuEFKDBshlJMQ4RYH1aHkhW&ved=0ahUKEwi57reVl-mGAxWTl5UCHWIpCXsQ4dUDCBU&uact=5&oq=d%C3%B3lar&gs_lp=Egdnd3Mtd2l6IgZkw7NsYXIyDxAjGIAEGCcYigUYRhiCAjILEAAYgAQYsQMYgwEyCxAAGIAEGLEDGIMBMgsQABiABBixAxiDATILEAAYgAQYsQMYgwEyCxAAGIAEGLEDGIMBMgsQABiABBixAxiDATIIEAAYgAQYsQMyCxAAGIAEGLEDGIMBMgsQABiABBixAxiDAUjeBlAAWKYEcAB4AJABAJgBbqABgwSqAQMxLjS4AQPIAQD4AQGYAgWgApEEwgIKECMYgAQYJxiKBcICDhAuGIAEGLEDGIMBGIoFwgIREC4YgAQYsQMY0QMYgwEYxwHCAgsQLhiABBjRAxjHAZgDAJIHAzEuNKAHyyo&sclient=gws-wiz")
 sleep(2)
 
 # Pegando o valor do dólar
@@ -27,6 +28,3 @@ dolar = driver.find_element(By.CSS_SELECTOR, ('.DFlfde.SwHCTb'))
 dolar_site = dolar.text
 dolar_site = float(dolar_site.replace(",","."))
 print('Dólar: ', dolar_site)
-
-# Fechando navegador
-driver.quit()
